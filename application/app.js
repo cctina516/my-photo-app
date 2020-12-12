f777bb8a42c862269caa600299dba84e8753c61f
cctina516 <christinayan1019@gmail.com>
2020-12-12 15:39:29 -0800

    update

--- application/app.js
+++ application/app.js
@@ -2,10 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//import express handlebars
 var handlebars = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var errorPrint = require('./helpers/debug/debugprinters').errorPrint;
+var requestPrint = require('./helpers/debug/debugprinters').requestPrint;
+
 
 var app = express();
 
@@ -26,10 +31,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+
+app.use((req, res, next) => { // call next to move to next function
+    requestPrint(req.url); // print URL other request coming in 
+    next();
+})
 
 app.use('/', indexRouter); // localhost:3000
 app.use('/users', usersRouter);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.render('error', {err_message: err})
+}); //middleware function
 
 //error function
 // app.use((err, req, resp, next)=>{
