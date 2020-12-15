0fd087178ba02227b6a7ebcfa8aed4edcb6f451f
cctina516 <christinayan1019@gmail.com>
2020-12-14 20:50:10 -0800

    redirection

--- application/routes/index.js
+++ application/routes/index.js
@@ -7,4 +7,16 @@ router.get('/', function(req, res, next) {
   res.render('index', {name: "Chuting Yan"});
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/registration', function(req, res, next) {
+  res.render('registration');
+});
+
+// router.post('/', (req, res, next) => {
+//   res.redirect('/login');
+// })
+
 module.exports = router;
--- application/routes/users.js
+++ application/routes/users.js
@@ -8,4 +8,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', (req, res, next) => {
+  console.log(req);
+  res.send('data');
+})
+
 module.exports = router;
